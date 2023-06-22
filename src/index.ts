e0069623829ff8d5de485cbf6ff2618912c77f5d
Danny Almeida <danny.almeida@pedidosya.com>
2023-06-22 00:21:59 -0400

    configure eslint + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Microservice Template with NodeJs');
+import express, { Express } from 'express';
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+app.listen(3000);
+
+function a() {
+  return console.log('algo');
+}
+
+function aa() {
+  return console.log('algo');
+}
+const b = () => {
+  console.log('a');
+};
