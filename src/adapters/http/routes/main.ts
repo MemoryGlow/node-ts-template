7ec7cc8023810f4f7dc8777967293ef8596fbd82
Danny Almeida <danny.almeida@pedidosya.com>
2023-06-30 16:19:06 -0400

    add dependency injection with axilix

--- src/adapters/http/routes/main.ts
+++ src/adapters/http/routes/main.ts
@@ -1,16 +1,9 @@
-import { Application, Request, Response } from 'express';
-import fs from 'fs';
-// import container from '@container';
-
-// get version from package.json
-const { version } = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+import { Application } from 'express';
+import container from '@container';
 
 export default function (app: Application): void {
+  const healthCheckController = container.resolve('healthCheckController');
+
   // health check
-  app.get('/', (req: Request, res: Response) => {
-    return res.status(200).json({
-      status: 'Running...',
-      version,
-    });
-  });
+  app.get('/', healthCheckController.main);
 }
--- src/container.ts
+++ src/container.ts
@@ -1,1 +1,11 @@
-// file to declare dependency injection container using awilix
+import { createContainer, asClass } from 'awilix';
+
+import HealthCheckController from '@adapters/http/controllers/HealthCheck.controller';
+
+const container = createContainer();
+
+container.register({
+  healthCheckController: asClass(HealthCheckController).singleton(),
+});
+
+export default container;
